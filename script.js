9bbf35effafe6163080fe147b500329b61450c34
Ryanqaz123 <43908954+Ryanqaz123@users.noreply.github.com>
2020-11-21 13:01:25 -0500

    thrid text box

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function updateUi() {
   meets.sort((x1, x2) => toMinutes(x1[0]) - toMinutes(x2[0])).forEach((x) => {
     const element = document.createElement('p');
     const hours = x[0][0];
-    element.innerHTML = `${hours % 12 || 12}:${x[0][1]} ${hours < 12 ? 'A' : 'P'}M - ${x[1]}`;
+    element.innerHTML = `${hours % 12 || 12}:${x[0][1]} ${hours < 12 ? 'A' : 'P'}M - ${x[1]} : ${x[2]}`;
     div.appendChild(element);
   });
 }
@@ -22,12 +22,14 @@ window.addEventListener('load', () => {
   addButton.addEventListener('click', () => {
     const time = timeInput.value.split(':');
     const code = codeInput.value;
-    if (time.length === 2 && code) {
-      meets.push([ time.map((x) => Number(x)), code ]);
+    const name = nameInput.value;
+    if (time.length === 2 && code && name) {
+      meets.push([ time.map((x) => Number(x)), code, name]);
       updateUi();
 
       timeInput.value = null;
       codeInput.value = '';
+      nameInput.value = '';
     }
   });
 });
