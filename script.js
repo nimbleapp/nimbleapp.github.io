b5a6f1250f2983a0be26b373a86d4d3a5d31959c
flamesdev <johannpayer123@gmail.com>
2021-03-08 20:12:18 -0500

    Reformatted code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Website data
 const saveApiVersion = 1;
 const MEET_TYPE = {
-  GOOGLE : 0,
-  CUSTOM : 1,
+  GOOGLE: 0,
+  CUSTOM: 1,
 };
 
 // User data
@@ -11,7 +11,10 @@ let doPlaySound = false;
 let meets = [];
 
 function saveData() {
-  localStorage.setItem('save', JSON.stringify({ doHideWarning, doPlaySound, meets, saveApiVersion }));
+  localStorage.setItem(
+    'save',
+    JSON.stringify({ doHideWarning, doPlaySound, meets, saveApiVersion })
+  );
 }
 
 function updateMeetUi() {
@@ -24,51 +27,55 @@ function updateMeetUi() {
     return time[0] * 60 + time[1];
   }
 
-  meets.sort((x1, x2) => toMinutes(x1.time) - toMinutes(x2.time)).forEach((x, i) => {
-    const rowElement = document.createElement('div');
-    rowElement.className = 'row';
+  meets
+    .sort((x1, x2) => toMinutes(x1.time) - toMinutes(x2.time))
+    .forEach((x, i) => {
+      const rowElement = document.createElement('div');
+      rowElement.className = 'row';
 
-    const colMdAutoElement = document.createElement('div');
-    colMdAutoElement.className = 'col-md-auto';
+      const colMdAutoElement = document.createElement('div');
+      colMdAutoElement.className = 'col-md-auto';
 
-    const textElement = document.createElement('p');
-    const [ hours, minutes ] = x.time;
-    textElement.innerHTML = `${hours % 12 || 12}:${(minutes <= 9 ? '0' : '') + minutes} ${hours < 12 ? 'A' : 'P'}M - ${x.name} : <i>${x.code}</i>`;
+      const textElement = document.createElement('p');
+      const [hours, minutes] = x.time;
+      textElement.innerHTML = `${hours % 12 || 12}:${
+        (minutes <= 9 ? '0' : '') + minutes
+      } ${hours < 12 ? 'A' : 'P'}M - ${x.name} : <i>${x.code}</i>`;
 
-    const colElement = document.createElement('div');
-    colElement.className = 'col';
+      const colElement = document.createElement('div');
+      colElement.className = 'col';
 
-    const imgElement = document.createElement('img');
-    imgElement.className = 'trashcan float-right';
-    imgElement.src = './res/img/trash.svg';
-    imgElement.addEventListener('click', () => {
-      meets.splice(i, 1);
-      updateMeetUi();
-      saveData();
-    });
-
-    [
-      [ 'over', 'In' ],
-      [ 'leave', 'Out' ],
-    ].forEach((x) => {
-      rowElement.addEventListener(`mouse${x[0]}`, () => {
-        imgElement.style.animation = `fade${x[1]} 0.2s ease-in-out forwards`;
+      const imgElement = document.createElement('img');
+      imgElement.className = 'trashcan float-right';
+      imgElement.src = './res/img/trash.svg';
+      imgElement.addEventListener('click', () => {
+        meets.splice(i, 1);
+        updateMeetUi();
+        saveData();
       });
-    });
 
-    colMdAutoElement.appendChild(textElement);
-    colElement.appendChild(imgElement);
-    rowElement.appendChild(colMdAutoElement);
-    rowElement.appendChild(colElement);
-    div.appendChild(rowElement);
-  });
+      [
+        ['over', 'In'],
+        ['leave', 'Out'],
+      ].forEach((x) => {
+        rowElement.addEventListener(`mouse${x[0]}`, () => {
+          imgElement.style.animation = `fade${x[1]} 0.2s ease-in-out forwards`;
+        });
+      });
+
+      colMdAutoElement.appendChild(textElement);
+      colElement.appendChild(imgElement);
+      rowElement.appendChild(colMdAutoElement);
+      rowElement.appendChild(colElement);
+      div.appendChild(rowElement);
+    });
 }
 
 window.addEventListener('load', () => {
   function updateVolumeButtonUi() {
     volumeIcon.src = `./res/img/volume${doPlaySound ? '-on' : '-off'}.svg`;
   }
-  
+
   const save = localStorage.getItem('save');
   if (save) {
     const json = JSON.parse(save);
@@ -81,12 +88,14 @@ window.addEventListener('load', () => {
       if (apiVersion < saveApiVersion) {
         switch (apiVersion) {
           case 0:
-            json.meets.forEach((x) => { x.type = MEET_TYPE.GOOGLE; });
+            json.meets.forEach((x) => {
+              x.type = MEET_TYPE.GOOGLE;
+            });
             break;
           default:
             break;
         }
-  
+
         json.saveApiVersion = apiVersion + 1;
         didMigrate = true;
       } else {
@@ -117,8 +126,8 @@ window.addEventListener('load', () => {
 
   typeDropdown.addEventListener('change', () => {
     const index = typeDropdown.selectedIndex;
-    nameInput.placeholder = `${[ 'Class', 'Site' ][index]} name`;
-    codeInput.placeholder = [ 'Code', 'Link' ][index];
+    nameInput.placeholder = `${['Class', 'Site'][index]} name`;
+    codeInput.placeholder = ['Code', 'Link'][index];
   });
 
   addButton.addEventListener('click', () => {
@@ -126,7 +135,8 @@ window.addEventListener('load', () => {
     const name = nameInput.value;
     let code = codeInput.value;
     if (time.length === 2 && name && code) {
-      const type = MEET_TYPE[typeDropdown.value === 'Google meet' ? 'GOOGLE' : 'CUSTOM'];
+      const type =
+        MEET_TYPE[typeDropdown.value === 'Google meet' ? 'GOOGLE' : 'CUSTOM'];
       if (type === MEET_TYPE.GOOGLE) {
         code = code.toLowerCase();
       } else if (!code.includes('http://') && !code.includes('https://')) {
@@ -136,7 +146,7 @@ window.addEventListener('load', () => {
       meets.push({
         name,
         code,
-        time : time.map((x) => Number(x)),
+        time: time.map((x) => Number(x)),
         type,
       });
       updateMeetUi();
@@ -149,7 +159,7 @@ window.addEventListener('load', () => {
   });
 
   function playSound() {
-    (new Audio('./res/audio/ring.mp3')).play();
+    new Audio('./res/audio/ring.mp3').play();
   }
 
   volumeButton.addEventListener('click', () => {
@@ -196,14 +206,26 @@ window.addEventListener('load', () => {
 
   let lastMinute;
   setInterval(() => {
-    const minuteTime = (new Date()).setSeconds(0, 0);
+    const minuteTime = new Date().setSeconds(0, 0);
     if (lastMinute !== minuteTime) {
       const date = new Date();
-      const currentMeets = meets.filter((x) => x.time[0] === date.getHours() && x.time[1] === date.getMinutes());
-      if (currentMeets.map((x) => (x.type === MEET_TYPE.GOOGLE ? `https://g.co/meet/${x.code}` : x.code)).filter((x) => !window.open(x)).forEach((x) => {
-        createWarningNotification('The pop-up to your meet was blocked. Allow pop-ups for this website to enable automatic window opening. '
-          + `Click <a href=${x} target="_blank">here<a> to join the meet.`, 'danger');
-      }));
+      const currentMeets = meets.filter(
+        (x) => x.time[0] === date.getHours() && x.time[1] === date.getMinutes()
+      );
+      if (
+        currentMeets
+          .map((x) =>
+            x.type === MEET_TYPE.GOOGLE ? `https://g.co/meet/${x.code}` : x.code
+          )
+          .filter((x) => !window.open(x))
+          .forEach((x) => {
+            createWarningNotification(
+              'The pop-up to your meet was blocked. Allow pop-ups for this website to enable automatic window opening. ' +
+                `Click <a href=${x} target="_blank">here<a> to join the meet.`,
+              'danger'
+            );
+          })
+      );
 
       if (currentMeets.length !== 0) {
         tryPlayingSound();
